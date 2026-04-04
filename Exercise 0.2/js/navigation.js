e54ce19ac21d6d39b284439ce57c44f6be196b69
sheanbot <162719331+sheanbot@users.noreply.github.com>
2026-04-04 15:53:57 +0800

    add comment

--- Exercise 0.2/js/navigation.js
+++ Exercise 0.2/js/navigation.js
@@ -3,8 +3,8 @@ function showPage(pageId) {
     const pages = document.querySelectorAll('.page');
 
     pages.forEach(page => {
-        page.classList.remove('active');
+        page.classList.remove('active');  //remove the 'active' class from all pages to hide them   
     });
 
-    document.getElementById(pageId).classList.add('active');
+    document.getElementById(pageId).classList.add('active'); //add the 'active' class to the selected page to show it   
 }
